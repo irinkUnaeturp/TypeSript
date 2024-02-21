774cc02126af39746d09d93d9d97115c93b85c4a
irinkUnaeturp <160249402+irinkUnaeturp@users.noreply.github.com>
2024-02-21 12:04:26 +0000

    add new promise

--- src/sleep.ts
+++ src/sleep.ts
@@ -1,12 +1,5 @@
-async function sleep(mills: number): Promise<void>{
-//let result = await mills
-//console.log(result)
-let promise = new Promise((resolve) => {
-    setTimeout(() => resolve, mills)
-    })
-    let result = await promise
-    
-      console.log(result)
+export async function sleep(mills: number): Promise<void> {
+  let promise = new Promise((resolve) => {setTimeout(() => resolve, mills)})
+ let result = await promise
+ console.log(result)
 }
-let t = Date.now()
-sleep(100).then(()=> console.log(Date.now() - t))// 100
