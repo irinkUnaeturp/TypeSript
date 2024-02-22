050a0310ed7b2803d1a569dcf1c376570bd50ecf
irinkUnaeturp <160249402+irinkUnaeturp@users.noreply.github.com>
2024-02-22 09:14:07 +0000

    promise that return result aftar (x) millisecinds

--- src/sleep.ts
+++ src/sleep.ts
@@ -1,5 +1,3 @@
 export async function sleep(mills: number): Promise<void> {
-  let promise = new Promise((resolve) => {setTimeout(() => resolve, mills)})
- let result = await promise
- console.log(result)
+   return new Promise((resolve) => setTimeout(resolve, mills))
 }
