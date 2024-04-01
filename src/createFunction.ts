58691e7869d5e5b71557c2e62c75e3e3d204dbc5
irinkUnaeturp <irinkap2003@icloud.com>
2024-04-01 11:25:26 +0300

    promblems

--- src/createFunction.ts
+++ src/createFunction.ts
@@ -55,27 +55,99 @@
 //  console.log(expect(5).notToBe(5)); // throws "Equal"
 
 //counter II
-type Counter = {
-    increment: () => number,
-    decrement: () => number,
-    reset: () => number,
+// type Counter = {
+//     increment: () => number,
+//     decrement: () => number,
+//     reset: () => number,
+// }
+
+// function createCounter(init: number): Counter {
+//     let Init = init
+//     return {
+//     increment: () => {
+//        return  Init = init + 1
+
+//     },
+//     decrement: () => {
+//         return Init = init -1
+//     },
+//     reset: () => {
+//         return Init = init
+//     }
+// }
+// };
+
+// const counter = createCounter(5)
+// console.log(counter.increment()); // 6
+// console.log(counter.reset()); // 5
+// console.log(counter.decrement()); // 4
+
+//Apply Transform Over Each Element in Array
+
+function map(arr: number[], fn: (n: number, i: number) => number): number[] {
+    const array = []
+    const value: number
+    for (let i = 0; i < arr.length; i++) {
+        const value = fn(arr[i], i)
+        array.push(value)
+    }
+
+    return array
 }
 
-function createCounter(init: number): Counter {
-    function increment(init) {
-        init += 1
-        console.log(init)
-    }
-    function decrement(init) {
-        return init -= 1
-    }
-    function reset(init) {
-        return init
-    }
-};
+//Filter Elenets from array
+// let arr = [0,10,20,30]
+// let fn = function moreThan10(n) { return n > 10; }
 
-const counter = createCounter(5)
-counter.increment(); // 6
-counter.reset(); // 5
-counter.decrement(); // 4
+// let filter = function (arr, fn){
+//     return arr.filter(fn)
+// }
+// console.log(filter(arr,fn))
+
+//Allow one function call
+// type Oncefn = (...args: JSONValuee[]) => JSONValuee | undefined
+
+// function once(x: Function): OnceFn {
+   
+//         let i = 1;
+//         return function (...args) {
+//             if (i) {
+//                i = 0;
+//                 return (x(...args))
+//             }
+//         };
+//     }
+    
+
+
+//  let x = (a,b,c) => (a + b + c)
+//  let oncefn = once(x)
+
+//  console.log(oncefn(1,2,3)); // 6
+//  console.log(oncefn(2,3,6)); // returns undefined without calling fn
+
+
+//Debounce
+// type A = (...args: string[]) => void
+
+// export function debounce(fn: A, t: number): A {
+//     let timer;
+//     return (...args) => {
+//         if (timer !== undefined) {
+//             clearTimeout(timer);
+//         }
+//         timer = setTimeout(() => fn(...args), t);
+//     }
+// };
+
+// const log = debounce(console.log, 100);
+// log('Hello'); // cancelled
+// log('Hello'); // cancelled
+// log('Hello'); // Logged at t=100ms
+
+
+
+
+
+
 
