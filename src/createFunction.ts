bb830a038683c5c2b5a73bfa1ef8ce1da8d32601
irinkUnaeturp <irinkap2003@icloud.com>
2024-04-01 23:10:10 +0300

    promblems

--- src/createFunction.ts
+++ src/createFunction.ts
@@ -83,17 +83,18 @@
 // console.log(counter.decrement()); // 4
 
 //Apply Transform Over Each Element in Array
+// const arr = [1,2,3]
+// const fn = function plusone(n) { return n + 1; }
+// function map(arr: number[], fn: (n: number, i: number) => number): number[] {
+//     const array: Array<number> = []
+//     for (let i = 0; i < arr.length; i++) {
+//         const value = fn(arr[i], i)
+//         array.push(value)
+//     }
 
-function map(arr: number[], fn: (n: number, i: number) => number): number[] {
-    const array = []
-    const value: number
-    for (let i = 0; i < arr.length; i++) {
-        const value = fn(arr[i], i)
-        array.push(value)
-    }
-
-    return array
-}
+//     return array
+// }
+// console.log(map(arr,fn))
 
 //Filter Elenets from array
 // let arr = [0,10,20,30]
@@ -108,7 +109,7 @@ function map(arr: number[], fn: (n: number, i: number) => number): number[] {
 // type Oncefn = (...args: JSONValuee[]) => JSONValuee | undefined
 
 // function once(x: Function): OnceFn {
-   
+
 //         let i = 1;
 //         return function (...args) {
 //             if (i) {
@@ -117,7 +118,7 @@ function map(arr: number[], fn: (n: number, i: number) => number): number[] {
 //             }
 //         };
 //     }
-    
+
 
 
 //  let x = (a,b,c) => (a + b + c)
@@ -145,9 +146,93 @@ function map(arr: number[], fn: (n: number, i: number) => number): number[] {
 // log('Hello'); // cancelled
 // log('Hello'); // Logged at t=100ms
 
+//Array Reduce Transformation
+
+// type Fn = (accum: number, curr: number) => number
+
+// function reduce(nums: number[], fn: Fn, init: number): number {
+//     let line = init
+//     for (let i = 0; i < nums.length; i++) {
+//         line = fn(line, nums[i])
+//     }
+//     return line
+
+// };
+// console.log(reduce([1,2,3,4],function sum(accum, curr) { return accum + curr; },0))
 
 
+//Function Composion
+// type A = (x: number) => number;
+
+// function compose(functions: A[]): A {
+
+//     return function(x) {
+//         for (let i = functions.length - 1; i >= 0; i--) {
+//             x = functions[i](x)
+//         }
+//         return x
+//     }
+// };
+
+// const fn = compose([x => x + 1, x => 2 * x])
+// console.log(fn(4)) // 9
+
+//Return Length of arguments Passed
+
+// function argumentsLength(...args): number {
+//     const leng = args.length
+//     return leng
+// };
+// console.log(argumentsLength(1, 2, 3))
+
+//Allow one Function call
 
 
+// function once(fn: Function) {
 
+//     return function (...args) {
+
+//         let i = 1;
+//         return function (...args) {
+//             if (i) {
+//                 i = 0;
+//                 return (fn(...args))
+//             }
+//         };
+//     }
+
+// }
+
+// let fn = (a, b, c) => (a + b + c)
+// let onceF = once(f)
+
+// console.log(onceFn(1, 2, 3)); // 6
+// console.log(onceFn(2, 3, 6)); // returns undefined without calling fn
+
+//Memoize
+function memoize(fn) {
+    
+    const ad = {};
+   
+    return function(this:any, ...args) {
+     const key = JSON.stringify(args);
+     
+     if (key in ad) {
+       return ad[key];
+     }
+     
+     const result = fn.apply(this, args);
+     ad[key] = result;
+     
+     return result;
+   }
+   
+ }
+ 
+ 
+ const memoizedSum = memoize(function(a, b) {
+   return a + b;
+ });
+ 
+ console.log(memoizedSum(2, 3)); // Output: 5
 
